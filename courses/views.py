cf37a6678d60ea6f99987ccbd4bbf507e1646956
nafly09 <juniorinterlyte95@hotmail.com>
2022-05-27 17:06:45 -0300

    feat: finished get course by id, patch course and delete course routes

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate
+from django.db import IntegrityError
 from django.forms import ValidationError
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
@@ -11,6 +12,7 @@ from rest_framework.status import (
     HTTP_401_UNAUTHORIZED,
     HTTP_403_FORBIDDEN,
     HTTP_422_UNPROCESSABLE_ENTITY,
+    HTTP_404_NOT_FOUND
 )
 
 
@@ -50,14 +52,51 @@ class CoursesView(APIView):
 
 class FilterCourses(APIView):
     def get(self, _: Request, course_id: None):
+        if not course_id:
+            return Response("OMEGALUL")
         try:
-            if course_id:
-                course = Courses.objects.get(uuid=course_id)
-
+            course = Courses.objects.get(uuid=course_id)
             serializer = CourseSerializer(course)
-
             return Response(serializer.data)
         except ValidationError:
             return Response({"message": "Please inform a valid UUID"}, HTTP_422_UNPROCESSABLE_ENTITY)
         except Courses.DoesNotExist:
-            return Response({"message": "Course does not exist"}, HTTP_422_UNPROCESSABLE_ENTITY)
+            return Response({"message": "Course does not exist"}, HTTP_404_NOT_FOUND)
+
+    def patch(self, request: Request, course_id: None):
+        try:
+            serializer = CourseSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            if course_id:
+                Courses.objects.filter(pk=course_id).update(
+                    **serializer.validated_data)
+                updated_course = Courses.objects.get(pk=course_id)
+                serializer = CourseSerializer(updated_course)
+                return Response(serializer.data, HTTP_201_CREATED)
+        except IntegrityError:
+            return Response({"message": "Course with the same name already exists."}, HTTP_422_UNPROCESSABLE_ENTITY)
+        except Courses.DoesNotExist:
+            return Response({
+                "message": "Course does not exist"
+            }, HTTP_404_NOT_FOUND)
+        except ValidationError:
+            return Response({"message": "Please inform a valid UUID"}, HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def delete(self, _, course_id=None):
+        try:
+            found_course = Courses.objects.filter(pk=course_id).first()
+
+            if not Courses.objects.filter(pk=course_id):
+                raise Courses.DoesNotExist
+
+            found_course.delete()
+
+            return Response("", HTTP_204_NO_CONTENT)
+
+        except Courses.DoesNotExist:
+            return Response({
+                "message": "Course does not exist"
+            }, HTTP_404_NOT_FOUND)
+        except ValidationError:
+            return Response({"message": "Please inform a valid UUID"}, HTTP_422_UNPROCESSABLE_ENTITY)
